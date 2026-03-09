11401028481c00b8801686f0ff012e85b7fd4ea5
UP240513 <up240513@alumnos.upa.edu.mx>
2026-03-09 15:21:47 -0600

    pokemon info on screen

--- app/pokemon/[name].tsx
+++ app/pokemon/[name].tsx
@@ -1,12 +1,38 @@
 import { useLocalSearchParams } from "expo-router";
-import React from "react";
+import { useEffect, useState } from "react";
 import { Text, View } from "react-native";
 
 export default function PokemonDetailsScreen() {
   const params = useLocalSearchParams();
+  const [PokemonData, setPokemonData] = useState(null);
+
+  useEffect(() => {
+    getPokemon();
+  }, []);
+
+  const getPokemon = async () => {
+    try {
+      const URL = `https://pokeapi.co/api/v2/pokemon/${params.name}`;
+      const response = await fetch(URL, {
+        method: "GET",
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setPokemonData(data);
+      } else {
+        console.log("Bad Request");
+      }
+    } catch (error) {
+      console.log("ocurrio un error");
+    }
+  };
+
   return (
     <View>
       <Text>{params.name}</Text>
+
+      {PokemonData && <Text>{JSON.stringify(PokemonData)}</Text>}
     </View>
   );
 }
--- components/PokemonCard.tsx
+++ components/PokemonCard.tsx
@@ -11,7 +11,7 @@ export default function PokemonCard(props: PokemonCardProps) {
   const pokemonImageURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
   return (
     <Pressable
-      onPress={() => router.push("/new-screen")}
+      onPress={() => router.push(`/pokemon/${props.name}`)}
       style={({ pressed }) => [
         styles.pressableStyle,
         pressed && {
