2e2d2357c34679856074440e158313de97344c8a
UP240513 <up240513@alumnos.upa.edu.mx>
2026-03-11 15:14:40 -0600

    buscar pokemons

--- app/index.tsx
+++ app/index.tsx
@@ -1,47 +1,54 @@
+import PokemoCard from "@/components/PokemonCard";
 import { useEffect, useState } from "react";
-import { ScrollView } from "react-native";
-import PokemonCard from "../components/PokemonCard";
+import { ScrollView, TextInput } from "react-native";
+
 interface Pokemon {
   name: string;
   url: string;
 }
 
 export default function Index() {
-  const [results, setResults] = useState<Pokemon[]>([]);
+  const [result, setResult] = useState<Pokemon[]>([]);
+  const [allPokemons, setAllPokemons] = useState<Pokemon[]>([]);
+
   useEffect(() => {
-    console.log("entre en pantalla");
     getPokemons();
   }, []);
 
   const getPokemons = async () => {
     try {
-      const URL = "https://pokeapi.co/api/v2/pokemon?limit=100&offset=0";
-      const response = await fetch(URL, {
-        method: "GET",
-      });
+      const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+      const response = await fetch(URL, { method: "GET" });
 
       if (response.ok) {
         const data = await response.json();
-        setResults(data.results);
+        setResult(data.results);
+        setAllPokemons(data.results);
       } else {
-        console.log("Bard Request");
+        console.log("Bad Request");
       }
     } catch (error) {
-      console.log("ocurrio un error");
+      console.log("Ocurrió un error");
+    }
+  };
+
+  const filterPokemon = (text: string) => {
+    if (text === "") {
+      setResult(allPokemons);
+    } else {
+      const arrayFiltered = allPokemons.filter((pokemon) =>
+        pokemon.name.toLowerCase().includes(text.toLowerCase()),
+      );
+      setResult(arrayFiltered);
     }
   };
 
   return (
     <ScrollView>
-      {results.map((item) => {
-        return (
-          <PokemonCard
-            key={item.name}
-            name={item.name}
-            url={item.url}
-          ></PokemonCard>
-        );
-      })}
+      <TextInput onChangeText={filterPokemon} placeholder="Buscar pokémon..." />
+      {result.map((item) => (
+        <PokemoCard key={item.name} name={item.name} url={item.url} />
+      ))}
     </ScrollView>
   );
 }
